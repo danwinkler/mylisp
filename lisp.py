dad5978039fa8f4512260633d8a796e14f9dcd02
Daniel Winkler <dan@danwink.com>
2014-12-11 08:54:59 -0600

    things are sort of working haha

--- lisp.py
+++ lisp.py
@@ -1,3 +1,29 @@
+types = ["STR", "INT"]
+
+class Symbol(str): pass
+
+class Value(object):
+    def __init__( self, val, type ):
+        self.val = val
+        self.type = type
+
+    def __repr__( self ):
+        return str( self.val )
+
+    def __nonzero__( self ):
+        return self.val
+
+class Proc(object):
+    def __init__( self, params, body, env ):
+        self.params = params
+        self.body = body
+        self.env = env
+
+    def __call__( self, *args ):
+        sub_env = Env( self.env )
+        sub_env.vals.update(zip(self.params,args))
+        return eval( self.body, sub_env )
+
 def parse( s ):
     return build_ast( tokenize( s ) )
 
@@ -28,6 +54,11 @@ def tokenize( s ):
                 current += c
     return tokens
 
+def choose_container( token ):
+    if not token[0].isdigit():
+        return Symbol( token )
+    else:
+        return Value( int(token), "INT" )
 
 def build_ast( tokens ):
     ast = []
@@ -42,45 +73,70 @@ def build_ast( tokens ):
                 ast.append( [] )
                 stack = [ast[1]]
         else:
-             stack[-1].append( token )
+            t = choose_container( token )
+            stack[-1].append( t )
     return ast
 
-def build_default_env():
-    env = {}
-    def define( x ):
-        env[x[0]] = value[1:]
-    env['define'] = define
-    env['*'] = lambda x: int(x[0])*int(x[1])
-    return env
+def run( text ):
+    return eval( parse( text ) )
 
-def run_ast( ast ):
-    env = build_default_env()
-    for i in ast:
-        print eval( i, env )
+class Env(object):
+    def __init__( self, outer=None ):
+        self.vals = {}
+        self.outer = outer
 
-def eval( x, env ):
-    if type( x ) == str or len( x ) == 1:
-        if x in env:
-            return env[x]
+    def find( self, var ):
+        if var in self.vals:
+            return self.vals[var]
+        elif self.outer is not None:
+            return self.outer.find( var )
+        else:
+            return None
+
+global_env = Env()
+global_env.vals.update({
+    "*": lambda x, y: Value( x.val*y.val, "INT" )
+})
+
+import traceback
+
+def eval( x, env=global_env ):
+    if isinstance( x, Symbol ):
+        v = env.find( x )
+        if v is not None:
+            return v
         else:
             return x
-    args = []
-    for i in xrange( 1, len( x )):
-        args.append( eval( x[i], env ) )
-
-    print x
-
-    return env[x[0]]( args )
-
-def run( text ):
-    return run_ast( parse( text ) )
+    elif isinstance( x, Value ):
+        return x
+    elif len(x) == 0:
+        return None
+    elif x[0] == 'if':
+        if eval( x[1], env ):
+            return eval( x[2], env )
+        else:
+            return eval( x[3], env )
+    elif x[0] == 'define':
+        env.vals[eval(x[1], env)] = eval( x[2], env )
+        return None
+    elif x[0] == 'lambda':
+        return Proc( x[1:-1], x[-1], env )
+    elif isinstance( x[0], list ):
+        return [eval( v, env ) for v in x]
+    else:
+        proc = eval( x[0], env )
+        args = [eval(v, env) for v in x[1:]]
+        return proc( *args )
 
 test = '''
-(define hello x y (* x y))
+(* 2 2)
+'''
+
+test1 = '''
+(define hello (lambda x y (* x y)))
 (hello 2 (* 2 2))
+
 
 '''
 
-print parse( test )
-
-print run( test )
+print run( test1 )
